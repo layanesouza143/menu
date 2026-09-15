1249571ef4d89bd8879f45dc623b84c036c5a720
20241TIIMI0259 <layane.fsouza14@gmail.com>
2026-09-15 11:05:57 -0300

    atualização 1

---
+++ app/src/main/java/com/layane/menu/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.layane.menu
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationView
+import com.layane.menu.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Hide the status bar.
+        windowCompaty.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, window.decorView).hide(WindowInsetsCompat.Type.statusBars())
+
+        setSupportActionBar(binding.toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.nav_open, R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // supportActionBar?.title = ""
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+    }
+}
